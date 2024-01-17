9251a323cf36c3c890fffce6b139f002e4507189
Matej Valocký <matej.valocky@gmail.com>
2024-01-17 22:41:49 +0100

    react1 033 Unikátní identifikátor, přidání tlačítka a zachycení kliknutí

--- src/components/AllTasks.js
+++ src/components/AllTasks.js
@@ -3,13 +3,20 @@ import "./AllTasks.css"
 import data from '../data'
 
 const AllTasks = () => {
+    const tasksHandler = () => {
+        console.log("klik");
+    }
+
+    
+
     return <div>
         {
             data.map((jednaUloha) => {
                 const {id, name} = jednaUloha // toto je destructuring
 
-                return <div className='one-task'>
+                return <div className='one-task' key={id}>
                     <h4>{name}</h4>
+                    <button onClick={tasksHandler}>vymazať</button>
                 </div>
             })
         }
