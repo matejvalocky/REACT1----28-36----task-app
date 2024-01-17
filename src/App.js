8deafe9a8b7ab0f08e624920516308cd9ef387d0
Matej Valocký <matej.valocky@gmail.com>
2024-01-17 22:08:44 +0100

    react1 030 funkcia map a ako pracovat s datami

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import React from 'react'
 
 const App = () => {
     // čistý javascript začiatok
-  
+  // const vychodziePole = [5, 6, 7, 8, 9]
+
+  // const vyslednePole = vychodziePole.map( (jednoCislo) => {
+  //   return jednoCislo + 10
+  // })
+
+
+  // console.log(vyslednePole)
 
 
      // čistý javascript koniec
+
+     const data = [
+      { id: 1, name: 'Vynést koš' },
+      { id: 2, name: 'Uklidit kuchyň' },
+      { id: 3, name: 'Zajít do fitka' },
+      { id: 4, name: 'Nakoupit' },
+      { id: 5, name: 'Nakrmit kočku' },
+  ]
+
+  const poleNazvov = data.map( (oneTask) => {
+      return oneTask.name
+  } )
+
+
+  console.log(poleNazvov)
 
 
   return (
