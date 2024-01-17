d6b2774f648130e28b17f8812738141b3de4b66e
Matej Valocký <matej.valocky@gmail.com>
2024-01-17 22:16:51 +0100

    react 1 031 funkcia filter

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React from 'react'
 
 const App = () => {
-    // čistý javascript začiatok
-  // const vychodziePole = [5, 6, 7, 8, 9]
-
-  // const vyslednePole = vychodziePole.map( (jednoCislo) => {
-  //   return jednoCislo + 10
-  // })
-
-
-  // console.log(vyslednePole)
-
-
-     // čistý javascript koniec
-
-     const data = [
-      { id: 1, name: 'Vynést koš' },
-      { id: 2, name: 'Uklidit kuchyň' },
-      { id: 3, name: 'Zajít do fitka' },
-      { id: 4, name: 'Nakoupit' },
-      { id: 5, name: 'Nakrmit kočku' },
+  // čistý javascript začiatok
+  const data = [
+    { id: 1, name: 'Vynést koš' },
+    { id: 2, name: 'Uklidit kuchyň' },
+    { id: 3, name: 'Zajít do fitka' },
+    { id: 4, name: 'Nakoupit' },
+    { id: 5, name: 'Nakrmit kočku' },
   ]
 
-  const poleNazvov = data.map( (oneTask) => {
-      return oneTask.name
-  } )
+    const vymazaneId = 3
 
 
-  console.log(poleNazvov)
+    const vyslednePole = data.filter ( (oneTask) => {
+      return oneTask.id != vymazaneId
+    })
+
+    console.log(vyslednePole)
+
+  // čistý javascript koniec
+
+
+
+
+
+
+
 
 
   return (
